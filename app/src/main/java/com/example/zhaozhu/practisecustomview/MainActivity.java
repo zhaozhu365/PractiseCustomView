4c3c2e6c61ca4e91722656a20088f54f9a57ab70
zhaozhu@luobotec.com <zhaozhu@luobotec.com>
2016-08-27 17:04:55 +0800

    Android 自定义view --简约的折线图:有一些细微的错误，比如Y轴的刻度应该从小到大

--- app/src/main/java/com/example/zhaozhu/practisecustomview/MainActivity.java
+++ app/src/main/java/com/example/zhaozhu/practisecustomview/MainActivity.java
@@ -2,15 +2,33 @@ package com.example.zhaozhu.practisecustomview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.zhaozhu.practisecustomview.customview02.SimpleLineChart;
+
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
+
+    private SimpleLineChart mSimpleLineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mSimpleLineChart = (SimpleLineChart) findViewById(R.id.simpleLineChart);
+        String[] xItem = {"1", "2", "3", "4", "5", "6", "7"};
+        String[] yItem = {"10k", "20k", "30k", "40k", "50k"};
+        if (mSimpleLineChart == null)
+            Log.e("wing", "null!!!!");
+        mSimpleLineChart.setXItem(xItem);
+        mSimpleLineChart.setYItem(yItem);
+        HashMap<Integer, Integer> pointMap = new HashMap();
+        for (int i = 0; i < xItem.length; i++) {
+            pointMap.put(i, (int) (Math.random() * 5));
+        }
+        mSimpleLineChart.setData(pointMap);
 
     }
 }
